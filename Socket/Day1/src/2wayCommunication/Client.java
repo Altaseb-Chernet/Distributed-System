d6808cddd076bd34e5f5be7077d6d44c08a1a9d2
altaseb-chernet <altacher227@gmail.com>
2026-03-20 10:58:45 +0300

    2 way communication

---
+++ Socket/Day1/src/2wayCommunication/Client.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket sc = new Socket("localhost", 4000);
+            System.out.println("Connected to server");
+
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader in = new BufferedReader(new InputStreamReader(sc.getInputStream()));
+            PrintWriter out = new PrintWriter(sc.getOutputStream(), true);
+
+            // Thread for receiving messages from server
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    String msg;
+                    while ((msg = in.readLine()) != null) {
+                        System.out.println("Server: " + msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            // Thread for sending messages to server
+            Thread sendThread = new Thread(() -> {
+                try {
+                    String msg;
+                    while ((msg = keyboard.readLine()) != null) {
+                        out.println(msg);
+                        if (msg.equalsIgnoreCase("exit")) {
+                            sc.close();
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            receiveThread.start();
+            sendThread.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Socket/Day1/src/2wayCommunication/Server.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(4000);
+            System.out.println("Server is waiting for client...");
+
+            Socket sc = serverSocket.accept();
+            System.out.println("Client connected");
+
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader in = new BufferedReader(new InputStreamReader(sc.getInputStream()));
+            PrintWriter out = new PrintWriter(sc.getOutputStream(), true);
+
+            // Thread for receiving messages from client
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    String msg;
+                    while ((msg = in.readLine()) != null) {
+                        System.out.println("Client: " + msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            // Thread for sending messages to client
+            Thread sendThread = new Thread(() -> {
+                try {
+                    String msg;
+                    while ((msg = keyboard.readLine()) != null) {
+                        out.println(msg);
+                        if (msg.equalsIgnoreCase("exit")) {
+                            sc.close();
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            receiveThread.start();
+            sendThread.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
