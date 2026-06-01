9669f0036825904e9b00feeba070c72521b31633
altaseb-chernet <altacher227@gmail.com>
2026-06-01 20:18:45 +0300

    feat: add App and Client classes for basic socket communication

---
+++ Socket/SocketApp/src/socketapp/Client.java
@@ -0,0 +1,41 @@
+package socketapp;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+
+        try (
+            Socket socket = new Socket("localhost", 1234);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(
+                socket.getOutputStream(), true);
+            BufferedReader console = new BufferedReader(
+                new InputStreamReader(System.in));
+        ) {
+
+            // Thread to read server messages
+            new Thread(() -> {
+                try {
+                    String response;
+                    while ((response = in.readLine()) != null) {
+                        System.out.println(response);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            // Send user input to server
+            String input;
+            while ((input = console.readLine()) != null) {
+                out.println(input);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
